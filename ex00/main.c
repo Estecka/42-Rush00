4189621ac705333a244d91f6bade437d1fa94b2e
Adrien Baur <abaur@student.42.fr>
2019-08-03 12:23:30 +0200

    Moved all files into a ex00 subfolder

--- ex00/main.c
+++ ex00/main.c
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/03 09:42:13 by abaur             #+#    #+#             */
-/*   Updated: 2019/08/03 12:05:16 by jokaruk-         ###   ########.fr       */
+/*   Updated: 2019/08/03 12:16:24 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@ void	rush(int width, int height);
 
 int		main(void)
 {
-	rush(-5, 1);
+	rush(5, 5);
 	return (0);
 }
